188df7cfb063a1d8539397ca71f124824b31d82d
LEO <baxodirmamatvaliev@gmail.com>
2026-08-10 00:12:42 +0900

    feat: Increment productViews on GET product by Id

--- src/services/Product.service.ts
+++ src/services/Product.service.ts
@@ -10,33 +10,46 @@ class ProductService {
   }
 
   public async getProducts(): Promise<any[]> {
+
     const result = await this.productModel.find({}).exec();
+
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
     return result;
   }
 
   public async getProduct(productId: string): Promise<any> {
     const id = shapeIntoMongooseObjectId(productId);
-    const result = await this.productModel.findById(id).exec();
-    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    const result = await this.productModel
+    .findByIdAndUpdate(id , { $inc: {productViews: 1}},{new: true}).exec();
+
+    if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
     return result;
   }
 
   public async createProduct(input: any): Promise<any> {
     try {
+        console.log("createProduct");
       const result = await this.productModel.create(input);
+      
       return result;
+
     } catch (err) {
+
       console.error("ERROR, model createProduct", err);
       throw new Errors(HttpCode.BAD_REQUEST, Message.USED_PRODUCT_NAME);
+
     }
   }
 
   public async updateProduct(productId: string, input: any): Promise<any> {
     const id = shapeIntoMongooseObjectId(productId);
+
     const result = await this.productModel
       .findByIdAndUpdate(id, input, { new: true })
       .exec();
+      
     if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
     return result;
   }
