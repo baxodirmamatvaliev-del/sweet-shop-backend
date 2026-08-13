9260014f1c9ed2a6fbbb18aaa2bbd432c645d878
LEO <baxodirmamatvaliev@gmail.com>
2026-08-13 22:19:56 +0900

    fix: jwt  import conflict, auth middleware fully working

--- src/services/Member.service.ts
+++ src/services/Member.service.ts
@@ -1,6 +1,7 @@
 import MemberModel from "../schema/Member.model";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import * as bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 class MemberService {
   private readonly memberModel;
@@ -26,26 +27,33 @@ class MemberService {
   }
 
   //  "login" bolamiz 
-  public async login(input: any): Promise<any> { //inputni bazadan tekshiramiz
-    const member = await this.memberModel 
+  public async login(input: any): Promise<any> {
+  const member = await this.memberModel
+    .findOne({ memberNick: input.memberNick })
+    .select("+memberPassword")
+    .exec();
 
-      .findOne({ memberNick: input.memberNick }) //bzadan memberNick ni input bilan tekchiramiz ekanda endi 
-      .select("+memberPassword") 
-      .exec();
-
-    if (!member) {
-      throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK); 
-    }
-
-    const isMatch = await bcrypt.compare(input.memberPassword, member.memberPassword); //inputdagi paswordni DD dagisi bulan tekshimizz
-
-    if (!isMatch) {
-      throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
-    }
-
-    member.memberPassword = ""; 
-    return member;
+  if (!member) {
+    throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
   }
+
+  const isMatch = await bcrypt.compare(input.memberPassword, member.memberPassword);
+
+  if (!isMatch) {
+    throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
+  }
+
+  member.memberPassword = "";
+
+  const token = jwt.sign(
+    { _id: member._id, memberType: member.memberType },
+    process.env.JWT_SECRET as string,
+    { expiresIn: "24h" }
+  );
+
+  return { member, token };
+}
+
 }
 
 export default MemberService;
